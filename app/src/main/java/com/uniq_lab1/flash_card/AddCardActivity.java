cc194635ead9a015078ee8e7f8f2ae73e9dd30b1
ralphy89 <ralphdumera00@gmail.com>
2023-04-06 18:26:26 -0400

    ImageView, editText

---
+++ app/src/main/java/com/uniq_lab1/flash_card/AddCardActivity.java
@@ -0,0 +1,75 @@
+package com.uniq_lab1.flash_card;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+        ImageView cancel_img = findViewById(R.id.cancel_button);
+        ImageView save_img = findViewById(R.id.save_button);
+
+        EditText question = findViewById(R.id.edit_text);
+        EditText answer1 = findViewById(R.id.edit_text1);
+        EditText answer2 = findViewById(R.id.edit_text2);
+        EditText answer3 = findViewById(R.id.edit_text3);
+
+
+        if(getIntent().getStringExtra("Question") != null) {
+            String question_text = getIntent().getStringExtra("Question");
+            String ans1 = getIntent().getStringExtra("Answer1");
+            String ans2 = getIntent().getStringExtra("Answer2");
+            String ans3 = getIntent().getStringExtra("Answer3");
+            if (question_text.length() > 0) {
+                question.setText(question_text);
+            }
+            if (ans1.length() > 0) {
+                answer1.setText(ans2);
+            }
+            if (ans2.length() > 0) {
+                answer2.setText(ans1);
+            }
+            if (ans3.length() > 0) {
+                answer3.setText(ans3);
+            }
+        }
+
+        cancel_img.setOnClickListener(view -> {
+            // Intent intent1 = new Intent(AddCardActivity.this, MainActivity.class);
+            finish();
+        });
+        save_img.setOnClickListener(view -> {
+            String quest, ans1, ans2, ans3;
+            quest = question.getText().toString();
+            ans1 = answer1.getText().toString();
+            ans2 = answer2.getText().toString();
+            ans3 = answer3.getText().toString();
+
+            Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+            if(quest.length() == 0)
+            {
+                Toast.makeText(AddCardActivity.this, "Must enter the question to continue", Toast.LENGTH_SHORT).show();
+            } else if (ans1.length() == 0 || ans2.length() == 0 || ans3.length() == 0) {
+                Toast.makeText(AddCardActivity.this, "Must enter question and the 3 answers to continue", Toast.LENGTH_SHORT).show();
+            } else
+            {
+                intent.putExtra("Question", quest);
+                intent.putExtra("answer1", ans1);
+                intent.putExtra("answer2", ans2);
+                intent.putExtra("answer3", ans3);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/uniq_lab1/flash_card/MainActivity.java
+++ app/src/main/java/com/uniq_lab1/flash_card/MainActivity.java
@@ -1,28 +1,113 @@
 package com.uniq_lab1.flash_card;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView questionText = findViewById(R.id.flashcard_question);
-        TextView answerText = findViewById(R.id.flashcard_answer);
 
-        questionText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "I clicked the question", Toast.LENGTH_LONG).show();
-                questionText.setVisibility(View.INVISIBLE);
-                answerText.setVisibility(View.VISIBLE);
-            }
+        setContentView(R.layout.activity_main);
+
+        TextView questionText = findViewById(R.id.flashcard_question);
+        TextView answerText1 = findViewById(R.id.flashcard_answer1);
+        TextView answerText2 = findViewById(R.id.flashcard_answer2);
+        TextView answerText3 = findViewById(R.id.flashcard_answer3);
+        ImageView add_button = findViewById(R.id.add_button);
+        ImageView edit_button = findViewById(R.id.edit_button);
+        //TextView [] list_answer = {answerText1, answerText2, answerText3};
+        // Activity Launcher
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        Snackbar snackbar = Snackbar.make(questionText, "New question added !", Snackbar.LENGTH_LONG);
+                        snackbar.setBackgroundTint(ContextCompat.getColor(this, R.color.green_100));
+                        snackbar.setTextColor(Color.WHITE);
+                        snackbar.show();
+                        Intent intent = result.getData();
+                        assert intent != null;
+                        String question = intent.getStringExtra("Question");
+                        String answer1 = intent.getStringExtra("answer1");
+                        String answer2 = intent.getStringExtra("answer2");
+                        String answer3 = intent.getStringExtra("answer3");
+                        questionText.setText(question);
+                        answerText1.setText(answer2);
+                        answerText2.setText(answer1);
+                        answerText3.setText(answer3);
+                    }
+                }
+        );
+
+        answerText1.setOnClickListener(view -> {
+
+
+            Toast.makeText(MainActivity.this, "Bravo !! Correct Answer",
+                        Toast.LENGTH_SHORT).show();
+            answerText1.setBackgroundColor(ContextCompat.getColor(this, R.color.green_100));
+            answerText2.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+            answerText3.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
         });
+
+        answerText2.setOnClickListener(view -> {
+
+            Toast.makeText(MainActivity.this, "OUPS !! Wrong Answer", Toast.LENGTH_SHORT).show();
+            answerText2.setBackgroundColor(ContextCompat.getColor(this, R.color.red_100));
+            answerText1.setBackgroundColor(ContextCompat.getColor(this, R.color.green_100));
+            answerText3.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+        });
+
+        answerText3.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "OUPS !! Wrong Answer", Toast.LENGTH_SHORT).show();
+            answerText3.setBackgroundColor(ContextCompat.getColor(this, R.color.red_100));
+            answerText1.setBackgroundColor(ContextCompat.getColor(this, R.color.green_100));
+            answerText2.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+        });
+
+
+        add_button.setOnClickListener(view -> {
+           Toast.makeText(MainActivity.this, "Adding new Card ", Toast.LENGTH_SHORT).show();
+           answerText1.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+           answerText2.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+           answerText3.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+           Intent intent1 = new Intent(MainActivity.this, AddCardActivity.class);
+           activityResultLauncher.launch(intent1);
+        });
+
+        edit_button.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "Editing Card ", Toast.LENGTH_SHORT).show();
+            answerText1.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+            answerText2.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+            answerText3.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_100));
+
+            String question = questionText.getText().toString();
+            String ans1 = answerText1.getText().toString();
+            String ans2 = answerText2.getText().toString();
+            String ans3 = answerText3.getText().toString();
+
+            Intent intent2 = new Intent(MainActivity.this, AddCardActivity.class);
+
+            intent2.putExtra("Question", question);
+            intent2.putExtra("Answer1", ans1);
+            intent2.putExtra("Answer2", ans2);
+            intent2.putExtra("Answer3", ans3);
+
+            activityResultLauncher.launch(intent2);
+        });
+
     }
 }
